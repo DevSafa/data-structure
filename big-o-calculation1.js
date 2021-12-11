ef1cc8fd9049686f50f04289bf24bcfab39e5f04
sbarkadev <sbarka.dev@gmail.com>
2021-12-11 02:40:08 +0100

    calculate the big O of a function step by step pffff

---
+++ big-o-calculation1.js
@@ -0,0 +1,20 @@
+
+function anotherFunChallenge(input){
+    let a = 5; //O(1)
+    let b = 10;//O(1)
+    let c = 50;//O(1)
+    //looping based on input
+    for(let i = 0 ; i < input ; i++){ //O(n)
+        let x = i +1 ; //O(n)
+        let y = i + 2; //O(n)
+        let z = i + 3; //O(n)
+    }
+    for(let j = 0 ; j < input ; i++){ //O(n)
+        let p = j * 2;//O(1)
+        let q = j * 2;//O(1)
+    }
+    let whoAmI = "I don't know";//O(1)
+}
+
+//BIG O(4 + 7n)
+//BIG O(n)
